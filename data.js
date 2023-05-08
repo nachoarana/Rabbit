c9e85e379e8143cd4015deb006da067da6f89fe6
Nacho Arana <jota.arana@gmail.com>
2023-05-08 08:13:44 -0300

    refactored
    habits rendered ok

---
+++ data.js
@@ -0,0 +1,18 @@
+const habits = [{name: "exercise", id: 15},
+    {name: "meditate", id: 1},
+    {name: "eat healthy", id: 2},
+    {name: "write journal", id: 3},
+    {name: "be grateful", id: 4},
+    {name: "have sex", id: 5},
+    {name: "writer fuction", id: 6},
+    {name: "learn something", id: 7},
+    {name: "play instrument", id: 8},
+    {name: "social media", id: 9},
+    {name: "code", id: 10},
+    {name: "personal project", id: 11},
+    {name: "read", id: 12},
+    {name: "play sport", id: 13},
+    {name: "drink 2lts water", id: 14},
+]
+
+export {habits}
--- index.js
+++ index.js
@@ -1,56 +1,81 @@
-const habitBtn1 = document.getElementById('habit-btn1')
-const switchBtn2 = document.getElementById('switch-btn2')
-const switchBtn3 = document.getElementById('switch-btn3')
-const switchBtnCustom = document.getElementById('switch-btn-custom')
+import {habits} from './data.js'
+
 const currentDate = document.getElementById('current-date')
-const addHabitBtn = document.getElementById('add-habit-btn')
 const addHabitModal = document.getElementById('add-habit-modal')
-const submitCustomHabitBtn = document.getElementById('submit-custom-habit-btn')
 const customHabitText = document.getElementById('custom-habit-text')
+const habitPicker = document.getElementById('habit-picker')
 
 
-habitBtn1.addEventListener('click', function(){
-    habitBtn1.style.backgroundColor = '#84ab4e';
-    habitBtn1.textContent = 'Done!'
-   })
+// Toma los names del array habit y los renderiza como botones
 
-habitBtn1.addEventListener('dblclick', function(){
-    habitBtn1.style.backgroundColor = '#AF3E4D'
-    habitBtn1.textContent = 'Pending'
-})
+ function renderHabitButtons(){
+    
+    for (let habit of habits) {
+        habitPicker.innerHTML += `
+        <button class="picker" id="picker-${habit.id}" data-button="${habit.name}">${habit.name}</button>     `       
+    }
+        habitPicker.innerHTML += `
+        <button class="picker custom-picker" id="add-habit-btn">custom habit</button> ` 
+ }
 
-switchBtn2.addEventListener('click', function(){
-    switchBtn2.style.backgroundColor = '#84ab4e';
-    switchBtn2.textContent = 'Done!'
-   })
+renderHabitButtons()
 
-switchBtn2.addEventListener('dblclick', function(){
-   switchBtn2.style.backgroundColor = '#AF3E4D'
-    switchBtn2.textContent = 'Pending'
-})
+// Click en habit picker, se agrega a la lista
 
-switchBtn3.addEventListener('click', function(){
-    switchBtn3.style.backgroundColor = '#84ab4e';
-    switchBtn3.textContent = 'Done!'
-   })
+habitPicker.addEventListener('click', function(e){
+    if ((e.target.dataset.button)){
+    renderSelectedHabit(e.target.dataset.button)
+    }})
 
-switchBtn3.addEventListener('dblclick', function(){
-    switchBtn3.style.backgroundColor = '#AF3E4D'
-    switchBtn3.textContent = 'Pending'
-})
 
-switchBtnCustom.addEventListener('click', function(){
-    switchBtnCustom.style.backgroundColor = '#84ab4e';
-    switchBtnCustom.textContent = 'Done!'
-   })
+function renderSelectedHabit(habitName){
+    const targetObject = habits.filter(function(target){
+        return target.name === habitName
+    })[0]
+        // document.getElementById("empty-repository").display.style = "none"
+        document.getElementById("habit-repository").innerHTML += `
+            <div class="habit-container">
+            <p class="habit-text">${targetObject.name}
+            <button type="button" id="habit-btn1">Pending</button>
+        </div>
+        `
+}
 
-switchBtnCustom.addEventListener('dblclick', function(){
-    switchBtnCustom.style.backgroundColor = '#AF3E4D'
-    switchBtnCustom.textContent = 'Pending'
-})
 
-addHabitBtn.addEventListener('click', function(){
+// Gets current date
+
+let date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let user = "Nacho"
+
+currentDate.innerHTML = `Hi ${user}! Today is ${day}-${month} <br>
+                        Day 4 of week 5
+`;
+
+// PENDIENTE switch daily habit to complete / pending
+
+// habitBtn1.addEventListener('click', function(){
+//     habitBtn1.style.backgroundColor = '#84ab4e';
+//     habitBtn1.textContent = 'Done!';
+
+//    })
+
+// habitBtn1.addEventListener('dblclick', function(){
+//     habitBtn1.style.backgroundColor = '#AF3E4D'
+//     habitBtn1.textContent = 'Pending'
+
+
+
+// Show custom habit modal, hide rest
+document.getElementById("add-habit-btn").addEventListener('click', function(){
     addHabitModal.style.display = 'block'
+    const allMain = document.getElementsByClassName('main')
+    for (let all of allMain){
+        all.style.display = "none"
+    }
 })
 
 submitCustomHabitBtn.addEventListener('click', function(){
@@ -60,14 +85,9 @@ submitCustomHabitBtn.addEventListener('click', function(){
     document.getElementById('custom-habit').style.display = 'flex'
     customHabitText.innerHTML = `${customInput} >>`
     addHabitBtn.style.display = 'none' 
+    const allMain = document.getElementsByClassName('main')
+    for (let all of allMain){
+        all.style.display = "block"
+    }
 })
 
-// Get current date
-
-let date = new Date();
-
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
-
-currentDate.textContent = `Hi Nacho! Today is ${day}-${month}`;
